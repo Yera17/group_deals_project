71c818c9b1382f2464edb1f8753262d0df9c2a06
Vidya V <vidya1141@gmail.com>
2024-08-22 18:06:12 +0200

    code in the evening 6:00PM

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
     @categories_with_images = {
         "music" => "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4",
         "video" => "https://images.unsplash.com/photo-1517336714731-489689fd1ca8",
-        "TV" => "https://images.unsplash.com/photo-1489533119213-66a5cd877091",
+        "TV" => "https://www.lg.com/nl/lg-experience/images/lg-lab/2023/what-is-a-smart-tv/lg-experience-lg-lab-what-is-a-smart-tv-key-visual.jpg",
         "sports" => "https://images.unsplash.com/photo-1517649763962-0c623066013b",
         "gaming" => "https://images.unsplash.com/photo-1535223289827-42f1e9919769",
         "podcast" => "https://images.unsplash.com/photo-1504386106331-3e4e71712b38",
