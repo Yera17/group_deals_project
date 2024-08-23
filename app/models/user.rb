0bd7592525f376f6bc834a3e1f40d619a859e92c
Vidya V <vidya1141@gmail.com>
2024-08-23 12:44:53 +0200

    After all the image upload for the deals

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :subscriptions, dependent: :destroy
+  has_many :subscriptions
   has_many :deals, dependent: :destroy
   has_many :received_subscriptions, through: :deals, source: :subscriptions, dependent: :destroy
   # Include default devise modules. Others available are:
