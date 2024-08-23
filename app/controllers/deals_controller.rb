30cfd190c08d29e9df2720b2914aa92a4d90a2ad
Yerkanat Salaly <erko.salaly@gmail.com>
2024-08-23 14:17:22 +0200

    fridayLunch

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -21,6 +21,8 @@ class DealsController < ApplicationController
   def show
     @user_deal = ""
     @deal = Deal.find(params[:id])
+    @approved_subs = @deal.subscriptions.where(approved: true)
+    @available_spots = @deal.number_of_users.to_i - @approved_subs.length
     if user_signed_in?
       current_user.subscriptions.each do |sub|
         if @deal == sub.deal
@@ -32,9 +34,29 @@ class DealsController < ApplicationController
     end
   end
 
+  def edit
+    @deal = Deal.find(params[:id])
+  end
+
+  def update
+    @deal = Deal.find(params[:id])
+    if @deal.update(deal_params)
+      redirect_to deal_path(@deal)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @deal = Deal.find(params[:id])
+    @deal.destroy
+    # No need for app/views/deals/destroy.html.erb
+    redirect_to root_path, status: :see_other
+  end
+
   private
 
   def deal_params
-    params.require(:deal).permit(:name, :price, :description, :category, :duration, :number_of_users)
+    params.require(:deal).permit(:name, :price, :description, :category, :duration, :number_of_users, :image_path)
   end
 end
